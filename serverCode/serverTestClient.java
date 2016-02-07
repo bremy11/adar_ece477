addae4ea446216526dbc0e9bfc3645f5a60b91ba
Brooks Remy <bman02467@pal-nat186-154-14.itap.purdue.edu>
2016-02-06 19:28:32 -0500

    client tests working

--- serverCode/serverTestClient.java
+++ serverCode/serverTestClient.java
@@ -143,12 +143,56 @@ public class serverTestClient {
             socket.close();
         }
     }
-    //11
+    
+    public void deleteWaypoint() throws IOException {
+        Socket socket = new Socket(serverAddress, 3112);
+        // Make connection and initialize streams
+        try{
+            in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            out = new PrintWriter(socket.getOutputStream(), true);
+            
+            out.println(12);
+            
+            JSONObject obj = new JSONObject();
+            obj.put("id", "4");
+            out.println(obj.toJSONString());
+            
+            //out.println(obj.toJSONString());
+            //System.out.println(obj.toJSONString());
+            
+            // Consume the initial welcoming messages from the server
+            String t;
+            Object ob = null;
+            JSONObject jsonObject;
+            JSONParser parser = new JSONParser();
+            
+            while((t = in.readLine()) !=null)
+            {
+                try
+                {
+                    System.out .println(t);
+                    ob = parser.parse(t);
+                    jsonObject = (JSONObject) ob;
+                    System.out.println(jsonObject.toJSONString());
+                }catch (Exception e) {
+                    System.out.println(e.toString());
+                    out.println(e.toString());
+                }
+            }
+        }catch (IOException e)
+        {
+            e.printStackTrace();
+        }finally
+        {
+            socket.close();
+        }
+    }
     
     public static void main(String[] args) throws Exception {
         serverTestClient n = new serverTestClient();
         //n.addWaypointTest();
-        n.updateWaypointAdj();
+        //n.updateWaypointAdj();
+        n.deleteWaypoint();
         n.getWaypointsTest();
         
     }
