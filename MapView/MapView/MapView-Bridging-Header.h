ae0d5e530327c8571495c4a36b57c6bbcb9e238e
Tessie <tmciner@purdue.edu>
2016-02-18 19:35:26 -0500

    edits as of 2/18

---
+++ MapView/MapView/MapView-Bridging-Header.h
@@ -0,0 +1,15 @@
+//
+//  MapView-Bridging-Header.h
+//  MapView
+//
+//  Created by Tessie McInerney on 2/16/16.
+//  Copyright © 2016 Tessie McInerney. All rights reserved.
+//
+
+#ifndef MapView_Bridging_Header_h
+#define MapView_Bridging_Header_h
+
+#import "SocketRocket/SRWebSocket.h"
+
+
+#endif /* MapView_Bridging_Header_h */
